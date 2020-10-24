63a153cc19be8d8114e26b7e74fbd363f9b1e17c
Jonathan Nilsson <jonathan@voysys.se>
2020-10-24 21:52:54 +0200

    Clean

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let native_app_glue_path = ndk_root.join(Path::new("sources/android/native_app_glue"));
     let vr_api_path = env::current_dir()?.join("thirdparty/VrApi/Include");
 
-    println!("cargo:rustc-link-search=lib");
+    println!("cargo:rustc-link-search=thirdparty/VrApi/Libs/Android/arm64-v8a/Release");
     println!("cargo:rustc-cdylib-link-arg=-lvrapi");
     println!("cargo:rustc-cdylib-link-arg=-lEGL");
     println!("cargo:rustc-cdylib-link-arg=-lGLESv3");
